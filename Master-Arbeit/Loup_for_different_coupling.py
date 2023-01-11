1957c67c64dd38c889754d40f86195edee06391b
sander <sander.stammbach@outloock.com>
2023-01-11 13:46:19 +0100

    bla bli

---
+++ Master-Arbeit/Loup_for_different_coupling.py
@@ -0,0 +1,218 @@
+# /bin/env/python
+from calendar import c
+from ctypes import c_char_p
+from email import message_from_file
+import imp
+from tkinter import messagebox
+from IPython.display import display
+from re import A, U
+from sys import displayhook
+import matplotlib.pyplot as plt
+import numpy as np
+from qiskit import QuantumCircuit, Aer, transpile, assemble
+from qiskit.visualization import plot_histogram
+from math import gcd
+from numpy.random import randint
+import pandas as pd
+from fractions import Fraction
+import qutip
+print("conda activate")
+from qutip import mesolve as mesolve
+from qutip import basis as basis
+print("import succesful")
+from qutip import tensor as tensor
+from qutip import dag as dag
+from qutip import steadystate as steadystate
+from qutip import *
+from qutip import ptrace 
+
+
+
+class Diverse_Loups():
+    def EnergieCalculator(g,H_free, Trans_12, Trans_13, Trans_23, a, nh,nf,nc,h,kb,gamma_h,gamma_c,kappa,c_op_list):
+
+        
+
+        H_int=h*g*(Trans_12*a.dag()+a*Trans_12.dag())
+
+        H=H_free+H_int
+        
+    
+        rho = steadystate(H, c_op_list) ######## Are you sure its with only photons H_free?
+        rho_f=rho.ptrace(1)
+
+        def D(c_op_list,rho):
+            D=[]
+            for i in range(6):
+                D.append(c_op_list[i]*rho*c_op_list[i].dag()-1/2*(c_op_list[i].dag()*c_op_list[i]*rho-rho*c_op_list[i].dag()*c_op_list[i]))
+            return D
+
+        Liste_von_Q=[] # ExpectValue for Thermal Energy
+
+        Liste_von_Q.append(np.trace(H_free*(D(c_op_list,rho)[0]+D(c_op_list,rho)[1])))
+        Liste_von_Q.append(-1*np.trace(H_free*(D(c_op_list,rho)[2]+D(c_op_list,rho)[3])))
+        Liste_von_Q.append(-1*np.trace(H_free*(D(c_op_list,rho)[4]+D(c_op_list,rho)[5])))
+        #Liste_von_Q.append(g)  g in der liste anfügen
+
+        float_list= list(np.float_(Liste_von_Q))
+        print(float_list)    
+        Liste_von_Q=float_list
+
+        return(Liste_von_Q)
+
+
+
+
+    def Funktion(nh,proj_1,proj_2,proj_3,H,nc,nf,gamma_h,gamma_c,kappa,A1,A2,A3,A4,A5,A6):
+        nh_list=[]
+        Trace_list=[]
+        
+        
+
+        gamma_1=(nh+1)*gamma_h #### unsicher wegen vorfaktor 1/2 
+        gamma_2=(nh)*gamma_h
+        gamma_3=(nc+1)*gamma_c
+        gamma_4=(nc)*gamma_c
+        kappa_5=(nf+1)*kappa ####goes to zero
+        kappa_6=(nf)*kappa
+
+        c_op_list=[]    
+        c_op_list.append(np.sqrt(gamma_1)*A1)
+        c_op_list.append(np.sqrt(gamma_2)*A2)
+        c_op_list.append(np.sqrt(gamma_3)*A3)
+        c_op_list.append(np.sqrt(gamma_4)*A4)
+        c_op_list.append(np.sqrt(kappa_5)*A5)
+        c_op_list.append(np.sqrt(kappa_6)*A6)
+            
+
+        rho = steadystate(H, c_op_list)
+        Trace_list.append(np.trace(proj_1*rho))
+        Trace_list.append(np.trace(proj_2*rho))
+        Trace_list.append(np.trace(proj_3*rho))
+        
+
+        float_list2= list(np.float_(Trace_list))
+        #print(float_list)    
+        Trace_list=float_list2
+
+        return Trace_list
+            
+
+        #A1=Trans_13
+        #A2=Trans_13.dag()
+        #A3=Trans_23
+        #A4=Trans_23.dag()
+        #A5=a
+        #A6=a.dag()
+        #c_op_list=[]
+        
+
+
+
+        #gamma_1=(nh+1)*gamma_h #### unsicher wegen vorfaktor 1/2 
+        #gamma_2=(nh)*gamma_h
+        #gamma_3=(nc+1)*gamma_c
+        #gamma_4=(nc)*gamma_c
+        #gamma_5=(nf+1)*gamma_f ####goes to zero
+        #gamma_6=(nf)*gamma_f
+
+
+        #c_op_list.append(np.sqrt(gamma_1)*A1)
+        #c_op_list.append(np.sqrt(gamma_2)*A2)
+        #c_op_list.append(np.sqrt(gamma_3)*A3)
+        #c_op_list.append(np.sqrt(gamma_4)*A4)
+        #c_op_list.append(np.sqrt(gamma_5)*A5)
+        #c_op_list.append(np.sqrt(gamma_6)*A6)
+
+       
+    def Entropy(nh,Trans_12,a, kb,h,g,H,H_free,nc,nf,gamma_h,gamma_c,kappa,Trans_13,Trans_23,omega_c,omega_h,omega_f):
+        A1=Trans_13
+        A2=Trans_13.dag()
+        A3=Trans_23
+        A4=Trans_23.dag()
+        A5=a
+        A6=a.dag()
+        
+        H_int=h*g*(Trans_12*a.dag()+a*Trans_12.dag())
+
+        H=H_free+H_int
+        
+        
+        
+        
+        gamma_1=(nh+1)*gamma_h #### unsicher wegen vorfaktor 1/2 
+        gamma_2=(nh)*gamma_h
+        gamma_3=(nc+1)*gamma_c
+        gamma_4=(nc)*gamma_c
+        kappa_5=(nf+1)*kappa ####goes to zero
+        kappa_6=(nf)*kappa
+        
+        c_op_list=[]    
+        c_op_list.append(np.sqrt(gamma_1)*A1)
+        c_op_list.append(np.sqrt(gamma_2)*A2)
+        c_op_list.append(np.sqrt(gamma_3)*A3)
+        c_op_list.append(np.sqrt(gamma_4)*A4)
+        c_op_list.append(np.sqrt(kappa_5)*A5)
+        c_op_list.append(np.sqrt(kappa_6)*A6)
+        def T(n,omega):
+            
+            T=h*omega/(kb*(np.log((1/n)+1)))
+            return T
+
+        rho = steadystate(H, c_op_list) ######## Are you sure its with only photons H_free?
+        rho_f=rho.ptrace(1)
+        def D(c_op_list,rho):
+            D=[]
+            for i in range(6):
+                D.append(c_op_list[i]*rho*c_op_list[i].dag()-1/2*(c_op_list[i].dag()*c_op_list[i]*rho-rho*c_op_list[i].dag()*c_op_list[i]))
+            return D
+
+        Liste_von_Q=[] # ExpectValue for Thermal Energy
+        Liste_von_Q_f=[]
+        Liste_von_Q_c=[]
+        Liste_von_Q_h=[]
+        Liste_von_Q.append(np.trace(H_free*(D(c_op_list,rho)[0]+D(c_op_list,rho)[1]))/(T(nh,omega_h))+np.trace(H_free*(D(c_op_list,rho)[2]+D(c_op_list,rho)[3]))/T(nc,omega_c)+np.trace(H_free*(D(c_op_list,rho)[4]+D(c_op_list,rho)[5]))/T(nf,omega_f))
+        Liste_von_Q.append(np.trace(H_free*(D(c_op_list,rho)[0]+D(c_op_list,rho)[1]))/(T(nh,omega_h)))#liste_von_Q_h
+        Liste_von_Q.append(np.trace(H_free*(D(c_op_list,rho)[2]+D(c_op_list,rho)[3]))/(T(nc,omega_c)))
+        Liste_von_Q.append(np.trace(H_free*(D(c_op_list,rho)[4]+D(c_op_list,rho)[5]))/(T(nf,omega_f)))
+            #Liste_von_Q.append(g)  g in der liste anfügen
+
+        float_list= list(np.float_(Liste_von_Q))
+        print(float_list)    
+        Liste_von_Entropy=float_list
+
+        return(Liste_von_Entropy)
+
+    def Photonnumber(nh2,a,proj_1,proj_2,proj_3,H,nc,nf,gamma_h,gamma_c,kappa,A1,A2,A3,A4,A5,A6):
+        
+        Trace_list=[]
+        
+        
+
+        gamma_1=(nh2+1)*gamma_h #### unsicher wegen vorfaktor 1/2 
+        gamma_2=(nh2)*gamma_h
+        gamma_3=(nc+1)*gamma_c
+        gamma_4=(nc)*gamma_c
+        kappa_5=(nf+1)*kappa ####goes to zero
+        kappa_6=(nf)*kappa
+
+        c_op_list=[]    
+        c_op_list.append(np.sqrt(gamma_1)*A1)
+        c_op_list.append(np.sqrt(gamma_2)*A2)
+        c_op_list.append(np.sqrt(gamma_3)*A3)
+        c_op_list.append(np.sqrt(gamma_4)*A4)
+        c_op_list.append(np.sqrt(kappa_5)*A5)
+        c_op_list.append(np.sqrt(kappa_6)*A6)
+            
+
+        rho = steadystate(H, c_op_list)
+        Trace_list.append
+
+        n=np.trace(a.dag()*a*rho)
+        float_list2= list(np.float_(Trace_list))
+        #print(float_list)    
+        Trace_list=float_list2
+
+
+
+        return n 
